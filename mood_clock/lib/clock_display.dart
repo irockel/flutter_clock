9959ce756862e35ff37e8f444d0c97dae15755ba
Ingo Rockel <irockel@1worldsync.com>
2020-01-03 17:11:02 +0100

    added single digit animation

--- mood_clock/lib/clock_display.dart
+++ mood_clock/lib/clock_display.dart
@@ -20,31 +20,52 @@ class ClockDisplay extends StatelessWidget {
     final minuteFontSize = MediaQuery.of(context).size.width / 5;
 
     // Date format for hour, minute and seconds
-    final hour = DateFormat(this.is24HourFormat ? 'HH' : 'hh').format(dateTime);
-    final minute = DateFormat('mm').format(dateTime);
-    final second = dateTime.second; //DateFormat('ss').format(dateTime);
-    final showFirst = dateTime.second % 2 == 0;
-    final oldSecond = second -1;
+    final hours = DateFormat(this.is24HourFormat ? 'HH' : 'hh').format(dateTime).split('');
+    final minutes = DateFormat('mm').format(dateTime).split('');
+    final seconds = DateFormat('ss').format(dateTime).split('');
 
     return Row(
       crossAxisAlignment: CrossAxisAlignment.baseline,
       textBaseline: TextBaseline.ideographic,
       children: [
-        Text(hour + " ",
-            style: TextStyle(fontSize: hourFontSize),
-            ),
-        Text(minute + " ",
-            style: TextStyle(fontSize: minuteFontSize),
-            ),
-        AnimatedCrossFade(duration: const Duration(seconds: 1),
-          firstChild: showFirst ? Text("$second") : Text("$oldSecond"),
-          secondChild: showFirst ? Text("$oldSecond") : Text("$second"),
-          crossFadeState: showFirst ? CrossFadeState.showFirst : CrossFadeState.showSecond,
-          firstCurve: Curves.easeInOut,
-          secondCurve: Curves.easeInOut,),
-
+        ClockDisplayDigit(digit: hours[0], style: new TextStyle(fontSize: hourFontSize)),
+        ClockDisplayDigit(digit: hours[1], style: new TextStyle(fontSize: hourFontSize)),
+        SizedBox(width: 30),
+        ClockDisplayDigit(digit: minutes[0], style: new TextStyle(fontSize: minuteFontSize)),
+        ClockDisplayDigit(digit: minutes[1], style: new TextStyle(fontSize: minuteFontSize)),
+        SizedBox(width: 30),
+        ClockDisplayDigit(digit: seconds[0]),
+        ClockDisplayDigit(digit: seconds[1]),
       ],
     );
   }
+}
 
+///
+/// display and animate a clock display digit
+///
+class ClockDisplayDigit extends StatelessWidget {
+  final String digit;
+  final TextStyle style;
+
+  ///
+  /// provide digit and optionally style to display
+  ///
+  ClockDisplayDigit({this.digit, this.style});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSwitcher(
+      duration: const Duration(milliseconds: 500),
+      switchInCurve: Curves.easeInCirc,
+      switchOutCurve: Curves.easeOutCirc,
+      child: Text("$digit", style: style, key: ValueKey(digit)),
+      transitionBuilder: (Widget child, Animation<double> animation) {
+        return ScaleTransition(
+          child: child,
+          scale: animation,
+        );
+      },
+    );
+  }
 }
